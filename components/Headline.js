5e8055a4793bfa4978f693ef7f49d7621b305576
kosuke-17 <tamutamu17@icloud.com>
2021-08-12 15:40:05 +0900

    ♻️ Main部分のコンポーネント化

--- components/Headline.js
+++ components/Headline.js
@@ -1,13 +1,15 @@
 import styles from "../styles/Home.module.css"
 
 export function Headline(props) {
+  console.log(props)
   return(
     <>
       <h1 className={styles.title}>{props.title} page</h1>
 
       <p className={styles.description}>
         Get started by editing{' '}
-        <code className={styles.code}>pages/{props.title}.js</code>
+        {props.children}
       </p>
-    </>  )
+    </>
+  )
 }
