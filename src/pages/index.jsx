67b62da768a8f0e0943e3cfa0060960dd8e19416
kosuke-17 <tamutamu17@icloud.com>
2021-08-14 19:26:14 +0900

    ⚗️ 試しにクリックイベントを記載

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,17 +3,38 @@ import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main'
 import { Header } from '../components/Header'
+import { useCallback } from 'react'
+
+// const handleClick = (e) => {
+//   console.log(e.target.href)
+//   e.preventDefault();
+// }
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Index Page</title>
-      </Head>
-      <Header/>
-      <Main page="index"  />
+  const foo = 1;
 
-      <Footer />
-    </div>
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href)
+    e.preventDefault();
+    alert(foo);
+  },  [])
+  
+  return (
+    <>
+      <div className={styles.container}>
+        <Head>
+          <title>Index Page</title>
+        </Head>
+        <Header/>
+        <a
+          href="/about"
+          onClick={handleClick}
+        >ボタン
+        </a>
+        <Main page="index"  />
+
+        <Footer />
+      </div>
+    </>
   )
 }
