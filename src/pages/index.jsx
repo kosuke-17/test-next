b0d62df90803b14be207cfeb1e3a09078a678305
kosuke-17 <tamutamu17@icloud.com>
2021-08-14 20:10:23 +0900

    ✨ カウンターを設置

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main'
 import { Header } from '../components/Header'
-import { useCallback, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 // const handleClick = (e) => {
 //   console.log(e.target.href)
@@ -11,40 +11,33 @@ import { useCallback, useEffect } from 'react'
 // }
 
 export default function Home() {
-  // const foo = 1;
+  const [count, setCount] = useState(1);
+  // let foo = 1;
 
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href)
-    e.preventDefault();
-    alert(foo);
-  },  [])
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  }
 
   useEffect(() => {
-    console.log("マウント時")
     document.body.style.backgroundColor = "lightblue";
-
     return () => {
-      console.log("アンマウント時")
       document.body.style.backgroundColor = "";
     }
   }, []);
 
+  console.log(count);
+
   return (
-    <>
       <div className={styles.container}>
         <Head>
           <title>Index Page</title>
         </Head>
         <Header/>
-        {/* <a
-          href="/about"
-          onClick={handleClick}
-        >ボタン
-        </a> */}
+        <h1>{count}</h1>
+        <button onClick={handleClick}>ボタン</button>
         <Main page="index"  />
-
         <Footer />
       </div>
-    </>
   )
 }
