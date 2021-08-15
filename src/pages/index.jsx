3d7d8d225df13bb59abeeeb859bafd739ada37d0
kosuke-17 <tamutamu17@icloud.com>
2021-08-15 15:53:52 +0900

    🎨 カスタムフックスを使ってうまくまとめた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,48 +3,14 @@ import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main'
 import { Header } from '../components/Header'
-import { useCallback, useEffect, useState } from 'react'
+import { useCounter } from 'src/hooks/useCounter'
+import { useInputArray } from 'src/hooks/useInputArray'
+import { useBgLightBlue } from 'src/hooks/useBgLightBlue'
 
 export default function Home() {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState("")
-  const [isShow, setIsShow] = useState(true)
-  const [array, setArray] = useState([])
-
-  const handleClick = useCallback(() => {
-    if (count < 10){
-    setCount((prevcount) => prevcount + 1);
-    }
-  }, [count])
-
-  const handleDisplay = useCallback(() =>{
-    setIsShow((previsShow) => !previsShow);
-  })
-
-  const handleChange = useCallback((e) => {
-    if (e.target.value.length > 5) {
-      alert("5文字以内にしてください");
-      return;
-    }
-    setText(e.target.value.trim());
-}, []);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some(item => item === text )) {
-        alert("おなじ要素が既に存在します");
-        return prevArray;
-      }
-      return [...prevArray, text];
-    });
-  }, [text])
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-    return () => {
-      document.body.style.backgroundColor = "";
-    }
-  }, []);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
 
   return (
       <div className={styles.container}>
