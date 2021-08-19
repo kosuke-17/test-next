fa574d995c46cd98497c3c73e74190b41c503441
kosuke-17 <tamutamu17@icloud.com>
2021-08-19 15:01:26 +0900

    🚚 投稿系ページの名前変更

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,12 +2,12 @@ import Head from 'next/head'
 import styles from 'src/styles/Home.module.css'
 import { Header } from 'src/components/Header'
 
-const Index = (props) => {
+const Index = () => {
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>About Page</title>
+        <title>Index Page</title>
       </Head>
       <Header/>
       <h1>Next.jsで学ぶReact講座</h1>
--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -1,19 +1,19 @@
 import Head from 'next/head'
 import styles from 'src/styles/Home.module.css'
 import { Header } from 'src/components/Header'
-import { Posts } from 'src/components/Posts'
+import { Posts as PostsComponents } from 'src/components/Posts'
 
 
-const Home = (props) => {
+const Posts = () => {
   return (
       <div className={styles.container}>
         <Head>
           <title>Index Page</title>
         </Head>
         <Header/>
-        <Posts />
+        <PostsComponents />
       </div>
   )
 }
 
-export default Home
+export default Posts
